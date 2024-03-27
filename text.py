a556ee72e9b9e4b6661d401ab3e574658e42311c
Yurilt <3369363412@qq.com>
2024-03-27 22:52:54 +0800

    modified:   text.py

--- text.py
+++ text.py
@@ -5,14 +5,15 @@ from random import random
 
 g = HaiLogs()
 for i in range(10000):
+    
     time.sleep(0)
     g.log_out()
 
-h = []
-for i in range(1000):
-    h.append(HaiVector([random(), random(), random()]))
+# h = []
+# for i in range(1000):
+#     h.append(HaiVector([random(), random(), random()]))
 
-for v in h:
-    print(v)
+# for v in h:
+#     print(v)
 
 input()
